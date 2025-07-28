bfa2fb2f3784f92b0ebfe0ecc2137c3f95e591c6
Cristhian Monasterio <cristhian.monasterio@gmail.com>
2025-07-28 11:25:29 -0400

    particioning

--- 02-user-permission- management.sql
+++ 02-user-permission- management.sql
@@ -8,6 +8,7 @@ show grants;
 show grants for current_user;
 show grants for myuser;
 
+-- Grant permissions
 grant select on mydatabase.* to 'myuser'@'localhost';
 grant select, insert on mydatabase.mytable to 'myuser'@'localhost';
 grant execute on procedure mysql.create_db to 'user1'@'host';
@@ -19,9 +20,11 @@ select User, Host from mysql.user;
 
 FLUSH PRIVILEGES;
 
+-- User and permission management
 alter user 'root'@'localhost' identified by '12345678';
 create user 'tom'@'localhost' identified by '1234';
 
+-- User and permission management
 create database db1;
 create table db1.tab1 (txt text);
 insert into db1.tab1 values ('hello');
@@ -32,11 +35,13 @@ show grants for 'tom'@'localhost';
 drop user 'tom'@'localhost';
 drop database db1;
 
+-- Create a role
 create role role_db1;
 create user 'db1_user1'@'localhost' identified by 'password';
 grant select, insert on db1.tab1 to role_db1;
 grant role_db1 to 'db1_user1'@'localhost';
 
+-- set role
 set role role_db1; --as db1_user1 into db
 select current_role; --as db1_user1 into db
 set default role role_db1; --as db1_user1 into db
@@ -44,5 +49,6 @@ set default role role_db1; --as db1_user1 into db
 set default role role_db1 for 'db1_user1'@'localhost';
 set default role none for 'db1_user1'@'localhost';
 
+-- show role grants
 show grants for 'db1_user1'@'localhost';
 show grants for role_db1;
